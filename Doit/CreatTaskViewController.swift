d247f89916727d55b81e05bdf493c39302b892cf
Viviana Benavides <viviana.ben.par@gmail.com>
2017-09-03 00:34:18 -0500

    it's working with core Data now! :)

--- Doit/CreatTaskViewController.swift
+++ Doit/CreatTaskViewController.swift
@@ -9,32 +9,32 @@
 import UIKit
 
 class CreatTaskViewController: UIViewController {
-
+    
     @IBOutlet weak var importantSwitch: UISwitch!
     @IBOutlet weak var taskNameTextField: UITextField!
     
-    var previousVC = TasksViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func addTapped(_ sender: Any) {
-        //Create a Task from the outlet information 
+        //Create a Task from the outlet information
         
-        let task = Task()
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let task = Task(context : context )
         task.name = taskNameTextField.text!
         task.important = importantSwitch.isOn
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
-        //Add new task to array in previous viewController
+        //Pop back
         
-        previousVC.tasks.append(task)
-        previousVC.tableView.reloadData()
         navigationController!.popViewController(animated: true)
     }
-   
     
-
+    
+    
 }
